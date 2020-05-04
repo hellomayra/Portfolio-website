36718fbc35d72cfe149de4f6c83bae236384fc7e
hellomayra <mayrareciod@gmail.com>
2020-05-04 14:47:18 +0200

    Navbar - toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
+
+
 console.log("Hi! Welcome to My Portfolio Site");
+
+function menuToggle() {
+    var x = document.getElementById('navbar-nav');
+    if (x.className === "navbar-nav") {
+        x.className += " responsive";
+      } else {
+        x.className = "navbar-nav";
+      }
+    }
